8aec1f309ff4abe233c09c004ebafeb555a39b35
OmarVargas235 <ov1356272@gmail.com>
2023-04-27 00:43:55 -0500

    feat: update

--- dist/assets/img2-311f80ec.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/img1-ac15277e.jpg",a="/assets/img2-2584dd24.jpg";export{a,s as i};
--- dist/assets/img4-8b0d6820.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/img3-c6fab152.jpg",a="/assets/img4-6f7f47cf.jpg";export{a,s as i};
